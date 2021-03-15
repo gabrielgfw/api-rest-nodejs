ec10595b60024cfe8ec063adf2089a8a5f5969f9
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2021-03-15 16:56:43 -0300

    createing providers and user controller

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,10 +1,12 @@
 import { User } from "../../entities/User";
+import { IMailProvider } from "../../providers/IMailProvider";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { ICreateUserDTO } from "./CreateUserDTO";
 
 export class CreateUserUseCase {
   constructor(
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+    private mailProvider: IMailProvider,
   ) {}
 
   async execute(data: ICreateUserDTO) {
@@ -16,5 +18,18 @@ export class CreateUserUseCase {
   
     const user = new User(data);
     await this.usersRepository.save(user);
+
+    this.mailProvider.sendMail({
+      to: {
+        name: data.name,
+        email: data.email,
+      },
+      from: {
+        name: "Email do App",
+        email: "email@app.com"
+      },
+      subject: "Teste de Título",
+      body: "<p>Baita teste de e-mail</p>"
+    })
   }
 }
