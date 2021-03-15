51736e0d3491e38c19186d1f648f620941e45ac8
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2021-03-15 16:42:17 -0300

    config user DTO and createUserUseCase

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,11 +1,20 @@
+import { User } from "../../entities/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { ICreateUserDTO } from "./CreateUserDTO";
 
 export class CreateUserUseCase {
   constructor(
     private usersRepository: IUsersRepository
   ) {}
 
-  async execute() {
-    const userAlreadyExists = await this.usersRepository.findByEmail();
+  async execute(data: ICreateUserDTO) {
+    const userAlreadyExists = await this.usersRepository.findByEmail(data.email);
+    
+    if(userAlreadyExists) {
+      throw new Error('User already exists.');
+    }
+  
+    const user = new User(data);
+    await this.usersRepository.save(user);
   }
 }
